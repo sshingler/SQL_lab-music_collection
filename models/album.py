e9767045145babca98b02c91a485170e9a1a23b2
SShingler <s.shingler@hotmail.co.uk>
2023-02-14 16:17:38 +0000

    3 - complete models

---
+++ models/album.py
@@ -0,0 +1,6 @@
+class Album: 
+    def __init__(self, title, genre, user, id = None): 
+        self.title = title 
+        self.genre = genre 
+        self.user = user 
+        self.id = id 
---
+++ models/artist.py
@@ -0,0 +1,5 @@
+class Artist: 
+    
+    def __init__(self, artist_name, id = None):
+        self.artist_name = artist_name
+        self.id = id 
