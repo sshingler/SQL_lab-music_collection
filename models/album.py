c4592f5a24d8242a2a7281b54efe8be1dce17225
SShingler <s.shingler@hotmail.co.uk>
2023-02-14 18:05:57 +0000

    6 - add save artist/album

--- models/album.py
+++ models/album.py
@@ -1,6 +1,6 @@
 class Album: 
-    def __init__(self, title, genre, user, id = None): 
+    def __init__(self, title, genre, artist, id = None): 
         self.title = title 
         self.genre = genre 
-        self.user = user 
+        self.artist = artist
         self.id = id 
--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -1,11 +1,10 @@
 from db.run_sql import run_sql
 from models.album import Album
-from models.artist import Artist
 import repositories.artist_repository as artist_repository
 
 def save(album):
-    sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s, %s) RETURNING id"
-    values = [album.title, album.genre, album.artist.id ]
+    sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s, %s) RETURNING *"
+    values = [album.title, album.genre, album.artist.id]
     results = run_sql(sql, values)
     id = results [0]['id']
     album.id = id
