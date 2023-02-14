4aef5259b1da0d2759a968078f193a038c924cf3
SShingler <s.shingler@hotmail.co.uk>
2023-02-14 16:08:36 +0000

    2 - complete DB items

---
+++ db/task_manager.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists (
+    id SERIAL PRIMARY KEY,
+    artist_name VARCHAR(255)
+); 
+
+CREATE TABLE albums ( 
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255), 
+    genre VARCHAR(255),
+    artist_id INT NOT NULL REFERENCES artists(id)
+); 
+
