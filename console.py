13b8a47f4921167d123e80c64bfa187496ef606d
SShingler <s.shingler@hotmail.co.uk>
2023-02-14 16:39:47 +0000

    5 - complete console.py

---
+++ console.py
@@ -0,0 +1,20 @@
+import pdb 
+from models.album import Album
+from models.artist import Artist
+import repositories.album_repository as album_repository 
+import repositories.artist_repository as artist_repository 
+
+artist_1 = Artist("AC/DC")
+artist_repository.save(artist_1)
+artist_2 = Artist("Led Zeppelin")
+artist_repository.save(artist_2)
+
+album_1 = Album ("Back in Black", "Rock", artist_1)
+album_repository.save(album_1)
+album_2 = Album ("Physical Graffiti", "Rock", artist_2)
+album_repository.save(album_2)
+
+
+
+
+pdb.set_trace()
